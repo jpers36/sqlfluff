be8a1bd0d602c2af0c3b6eb37ccf65ceaf94ca98
jpersons <jpersons@iuhealth.org>
2022-01-31 15:20:42 -0500

    TSQL: Add Indentation recommendations

--- src/sqlfluff/dialects/dialect_tsql.py
+++ src/sqlfluff/dialects/dialect_tsql.py
@@ -1327,7 +1327,7 @@ class IfClauseSegment(BaseSegment):
     type = "if_clause"
 
     match_grammar = Sequence(
-        "IF", Indent, Ref("ExpressionSegment"), Dedent,
+        "IF", Ref("ExpressionSegment"), 
     )
 
 
